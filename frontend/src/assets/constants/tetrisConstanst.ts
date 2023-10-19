fc780d323f8968e31281dad2f3dbe8f4fdd13b13
Marcos Lopez <marcoslopezdev18@gmail.com>
2023-10-19 11:57:22 -0300

    fix response for mobile and large screen's

--- frontend/src/assets/constants/tetrisConstanst.ts
+++ frontend/src/assets/constants/tetrisConstanst.ts
@@ -4,15 +4,22 @@ import { PieceColor } from "@app/data/models/tetris/PieceColor";
 import { PieceType } from "@app/data/models/tetris/PieceType.enum";
 
 export const BLOCK_SIZE: number = 25;
-export const BLOCK_MOBILE_SIZE: number = 20;
+export const BLOCK_SIZE_LARGE: number = 40;
 
-export const BOARD_WIDTH: number = 12;
 export const BOARD_MOBILE_WIDTH: number = 10;
+export const BOARD_WIDTH: number = 12;
+export const BOARD_LARGE_WIDTH: number = 14;
 
+export const BOARD_MOBILE_HEIGHT: number = 16;
 export const BOARD_HEIGHT: number = 22;
-export const BOARD_MOBILE_HEIGHT: number = 20;
+export const BOARD_LARGE_HEIGHT: number = 24;
 
-export const NEXT_PIECE_SIZE: number = 18;
+
+
+export const NEXT_PIECE_SIZE_NORMAL = 18;
+export const NEXT_PIECE_SIZE_LARGE = 30;
+
+export const NEXT_PIECE_SIZE: number = (window.innerWidth > 1500) ? NEXT_PIECE_SIZE_LARGE : NEXT_PIECE_SIZE_NORMAL;
 
 export const NEXT_PIECE_WIDTH: number = 5;
 export const NEXT_PIECE_HEIGHT: number = 5;
@@ -21,9 +28,13 @@ export const LINE_WIDTH_SCALE = 0.01;
 export const SHADOW_BLUR_SCALE = 1;
 export const NEXT_POSITION = 1;
 
-export const BLOCK_SIZE_SCREEN = (window.innerWidth <= 500) ? BLOCK_MOBILE_SIZE : BLOCK_SIZE;
-export const BOARD_WIDTH_SCREEN = (window.innerWidth <= 500) ? BOARD_MOBILE_WIDTH : BOARD_WIDTH;
-export const BOARD_HEIGHT_SCREEN = (window.innerWidth <= 500) ? BOARD_MOBILE_HEIGHT : BOARD_HEIGHT;
+export const BLOCK_SIZE_SCREEN = (window.innerWidth > 1500) ? BLOCK_SIZE_LARGE : BLOCK_SIZE;
+
+export const BOARD_WIDTH_SCREEN = (window.innerWidth <= 500) ? BOARD_MOBILE_WIDTH : 
+                (window.innerWidth > 1500) ? BOARD_LARGE_WIDTH : BOARD_WIDTH;
+
+export const BOARD_HEIGHT_SCREEN = (window.innerWidth <= 500) ? BOARD_MOBILE_HEIGHT : 
+                  (window.innerWidth > 1500) ? BOARD_LARGE_HEIGHT : BOARD_HEIGHT;
 
 export const ACTIONS: Record<string, ACTION> = {
     "ArrowLeft": ACTION.LEFT,
