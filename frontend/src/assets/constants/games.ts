80c5fc3928e65461037fdc7a9be7d2edcf83c210
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-07-11 22:15:32 -0300

    refactor: change local img for cdn img

--- frontend/src/assets/constants/games.ts
+++ frontend/src/assets/constants/games.ts
@@ -10,7 +10,7 @@ export interface iGAMES {
 export const GAMES: iGAMES[] = [
   {
     ID: 'TETRIS-GAME',
-    IMG: '../../../../assets/images/tetris-frontpage.webp',
+    IMG: 'https://res.cloudinary.com/dd8ep4kjo/image/upload/v1720746510/tetris-frontpage_xafjo0.webp',
     NAME: 'Tetris',
     ALT: 'Tetris front page image',
     PATH: '/tetris',
@@ -18,21 +18,21 @@ export const GAMES: iGAMES[] = [
   },
   {
     ID: 'SNAKE-GAME',
-    IMG: '../../../../assets/images/snake-frontpage.webp',
+    IMG: 'https://res.cloudinary.com/dd8ep4kjo/image/upload/v1720746519/snake-frontpage_g4z1wy.webp',
     NAME: 'Snake Clasic',
     ALT: 'Snake Clasic front page image',
     PATH: '/snakeling',
   },
   {
     ID: 'TTT-GAME',
-    IMG: '../../../../assets/images/ttt-frontpage.webp',
+    IMG: 'https://res.cloudinary.com/dd8ep4kjo/image/upload/v1720746514/ttt-frontpage_uxcqzv.webp',
     NAME: 'Tic Tac Toe',
     ALT: 'Tic Tac Toe front page image',
     PATH: '/tictactoe',
   },
   {
     ID: 'MEMORAMA-GAME',
-    IMG: '../../../../assets/images/memorama-frontpage.png',
+    IMG: 'https://res.cloudinary.com/dd8ep4kjo/image/upload/v1720746524/memorama-frontpage_fvdiis.webp',
     NAME: 'Memorama',
     ALT: 'Memorama front page image',
     PATH: '/memorama',
@@ -40,7 +40,7 @@ export const GAMES: iGAMES[] = [
   },
   {
     ID: 'CHESS-GAME',
-    IMG: '../../../../assets/images/chess-frontpage.webp',
+    IMG: 'https://res.cloudinary.com/dd8ep4kjo/image/upload/v1720746534/chess-frontpage_hkkgpj.webp',
     NAME: 'Chess',
     ALT: 'Chess front page image',
     PATH: '/chess',
---
+++ frontend/src/assets/constants/util.ts
@@ -0,0 +1,2 @@
+export const CLOUDINARY_URL = (imgId: string) =>
+  `https://res.cloudinary.com/dd8ep4kjo/image/upload/v1720746534/${imgId}`;
